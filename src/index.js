e9666d5ec0b0085d1f5a8cb83e094254090bb2de
Ivaylo Ivanov <ivaylo.ivanov.ipi@gmail.com>
2017-11-12 23:31:07 +0100

    axios over request

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ var history = createHashHistory()
 
 var routes = require('./routes')
 
-var request = require('request');
+var axios = require('axios');
 
-request.get('https://mas-o-menos.herokuapp.com/api/ping')
+axios.get('https://mas-o-menos.herokuapp.com/api/ping')
 
 render(<Router history={history} render={applyRouterMiddleware(useScroll())} routes={routes}/>, document.getElementById('app'))
